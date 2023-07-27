21295a706c41a8a82b7b06170d27e35438b14ae7
Sanchit Arora <Sanchit.Arora@cybergindia.local>
2023-07-27 10:50:27 +0530

    add admin page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,13 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult User()
+    {
+        return View();
+    }
+    
+
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
