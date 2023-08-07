b702206e7884811704928498f6d70480c10867c1
Jashandeep Singla <jashandeepsingla24@gmail.com>
2023-08-07 11:11:08 +0530

    updated the dropdown and the device status

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -66,37 +66,55 @@ namespace final.Controllers
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
-        [HttpPost]
-        [Route("/Home/Index")]
-        public ActionResult Login(Users user)
+    [HttpPost]
+    [Route("/Home/Index")]
+    public ActionResult Login(Users user)
+    {
+        if (string.IsNullOrEmpty(user.UserName) || string.IsNullOrEmpty(user.Password))
         {
-            if (string.IsNullOrEmpty(user.UserName) || string.IsNullOrEmpty(user.Password))
-            {
-                return View("Index");
-            }
-            string desiredDomain = "@cginfinity.com";
-            if (!user.UserName.EndsWith(desiredDomain, StringComparison.OrdinalIgnoreCase))
-            {
-                return BadRequest("Invalid domain.");
-            }
-            var users = _context.Users.FirstOrDefault(u => u.UserName == user.UserName);
-            if (users == null)
-            {
-                return BadRequest("Invalid user");
-            }
-            bool VerifyPass = BCrypt.Net.BCrypt.Verify(user.Password, users.Password);
-            if (users != null && VerifyPass)
-            {
-                var token = GenerateJwtToken(users);
-                _accessor.HttpContext.Session.SetString("jwtToken", token);
-                var Id = users.U_Id;
-                _accessor.HttpContext.Session.SetString("UserId", Id.ToString());
-                var Name = users.UserName;
-                _accessor.HttpContext.Session.SetString("Name" , Name);
-                return RedirectToAction("Admin");
-            }
-            return BadRequest("Invalid");
+            return View("Index");
         }
+
+        string desiredDomain = "@cginfinity.com";
+        if (!user.UserName.EndsWith(desiredDomain, StringComparison.OrdinalIgnoreCase))
+        {
+            return BadRequest("Invalid domain.");
+        }
+
+        var users = _context.Users.FirstOrDefault(u => u.UserName == user.UserName);
+        if (users == null)
+        {
+            return BadRequest("Invalid user");
+        }
+
+        bool VerifyPass = BCrypt.Net.BCrypt.Verify(user.Password, users.Password);
+        if (!VerifyPass)
+        {
+            return BadRequest("Invalid password");
+        }
+
+        var employee = _context.Employee.FirstOrDefault(e => e.U_Id == users.U_Id);
+        if (employee == null)
+        {
+            return BadRequest("Employee record not found");
+        }
+
+        var token = GenerateJwtToken(users);
+        _accessor.HttpContext.Session.SetString("jwtToken", token);
+        var Id = users.U_Id;
+        _accessor.HttpContext.Session.SetString("UserId", Id.ToString());
+        var Name = users.UserName;
+        _accessor.HttpContext.Session.SetString("Name", Name);
+
+        if (employee.IsAdmin)
+        {
+            return RedirectToAction("Admin");
+        }
+        else
+        {
+            return RedirectToAction("User");
+        }
+    }
 
         [HttpPost]
         [Route("Admit")]
@@ -165,6 +183,149 @@ namespace final.Controllers
 
         }
 
+        [HttpGet]
+        [Route("/Home/Admin/ShowAssignedDevices")]
+        public IActionResult getAssignedDevices()
+        {
+            var assignedDevices = (from i in _context.Inventory
+                                join d in _context.Devices on i.D_Id equals d.D_Id
+                                where i.D_State == 'A' // Filter only assigned devices
+                                select new
+                                {
+                                        serialNo = i.Serial,
+                                        inventoryId = i.Id,
+                                        specification=i.Specifications,
+                                        deviceType = d.D_Type,
+                                }).ToList();
+            return Json(assignedDevices);
+        }
+
+        [HttpGet]
+        [Route("/Home/Admin/ShowUnassignedDevices")]
+        public IActionResult getUnassignedDevices()
+        {
+            var assignedDevices = (from i in _context.Inventory
+                                join d in _context.Devices on i.D_Id equals d.D_Id
+                                where i.D_State == 'U' // Filter only assigned devices
+                                select new
+                                {
+                                    serialNo = i.Serial,
+                                    inventoryId = i.Id,
+                                    specification=i.Specifications,
+                                    deviceType = d.D_Type,
+                                }).ToList();
+            return Json(assignedDevices);
+        }
+        [HttpGet]
+        [Route("/Home/Admin/ShowRepairDevices")]
+        public IActionResult getRepairDevices()
+        {
+            var assignedDevices = (from i in _context.Inventory
+                                join d in _context.Devices on i.D_Id equals d.D_Id
+                                where i.D_State == 'R' // Filter only assigned devices
+                                select new
+                                {
+                                    serialNo = i.Serial,
+                                    inventoryId = i.Id,
+                                    specification=i.Specifications,
+                                    deviceType = d.D_Type,
+                                }).ToList();
+            return Json(assignedDevices);
+        }
+
+        [HttpGet]
+        [Route("/Home/Admin/ShowDevicesByType")]
+        public IActionResult GetDevicesByType(string deviceType)
+        {
+            if (string.IsNullOrEmpty(deviceType))
+            {
+                return BadRequest("Device type cannot be empty.");
+            }
+            var devices = (from i in _context.Inventory
+                    join d in _context.Devices on i.D_Id equals d.D_Id
+                    where d.D_Type.ToLower() == deviceType.ToLower()
+                    select new
+                    {
+                            deviceType = d.D_Type,
+                        specification = i.Specifications,
+                        inventoryId = i.Id,
+                        serialNo = i.Serial
+                    }).ToList();
+            if (devices.Count == 0)
+            {
+                return NotFound("No devices found for the specified device type.");
+            }
+            return Json(devices);
+        }
+
+        [HttpGet]
+        [Route("/Home/Admin/ShowAssignedDevicesByType")]
+        public IActionResult getAssignedDevicesByType(string deviceType)
+        {
+            if (string.IsNullOrEmpty(deviceType))
+            {
+               return BadRequest("Device type cannot be empty.");
+            }
+            var assignedDevices = (from i in _context.Inventory
+                            join d in _context.Devices on i.D_Id equals d.D_Id
+                            where d.D_Type.ToLower() == deviceType.ToLower()
+                                    && i.D_State == 'A' // Filter only assigned devices
+                            select new
+                            {
+                                serialNo = i.Serial,
+                                inventoryId = i.Id,
+                                specification = i.Specifications,
+                                deviceType = d.D_Type,
+                            }).ToList();
+            return Json(assignedDevices);
+        }
+        
+        [HttpGet]
+        [Route("/Home/Admin/ShowUnassignedDevicesByType")]
+        public IActionResult getUnassignedDevicesByType(string deviceType)
+        {
+            if (string.IsNullOrEmpty(deviceType))
+            {
+               return BadRequest("Device type cannot be empty.");
+            }
+            var unassignedDevices = (from i in _context.Inventory
+                                join d in _context.Devices on i.D_Id equals d.D_Id
+                                where d.D_Type.ToLower() == deviceType.ToLower()
+                                    && i.D_State == 'U' // Filter only unassigned devices
+                                select new
+                                {
+                                    serialNo = i.Serial,
+                                    inventoryId = i.Id,
+                                    specification = i.Specifications,
+                                    deviceType = d.D_Type,
+                                }).ToList();
+            return Json(unassignedDevices);
+        }
+        
+        [HttpGet]
+        [Route("/Home/Admin/ShowRepairDevicesByType")]
+        public IActionResult getRepairDevicesByType(string deviceType)
+        {
+            if (string.IsNullOrEmpty(deviceType))
+            {
+                return BadRequest("Device type cannot be empty.");
+            }
+        var repairDevices = (from i in _context.Inventory
+                            join d in _context.Devices on i.D_Id equals d.D_Id
+                            where d.D_Type.ToLower() == deviceType.ToLower()
+                                && i.D_State == 'R' // Filter only repair devices
+                            select new
+                            {
+                                serialNo = i.Serial,
+                                inventoryId = i.Id,
+                                specification = i.Specifications,
+                                deviceType = d.D_Type,
+                            }).ToList();
+            return Json(repairDevices);
+        }
+
+ 
+
 
         [HttpPost]
         [Route("/Home/Admin/Assigned")]
@@ -179,6 +340,7 @@ namespace final.Controllers
                 return Json(new { success = false, message = "Employee or inventory not found" });
             }
             // Create an entry in the Assigned table to track the assignment
+            inventory.D_State = 'A';
             var assigned = new Assigned
             {
                 Emp_Code = assign.empCode,
@@ -186,11 +348,14 @@ namespace final.Controllers
                 AssignedBy = user,
                 AssignedAtUTC = DateTime.Now
             };
+            _context.Inventory.Update(inventory);
             _context.Assigned.Add(assigned);
             _context.SaveChanges();
 
             return Json(new { success = true }); ;
         }
+
+        
 
         [HttpGet]
         [Route("/Home/Admin/GetAssignedDevices/{empCode}")]
@@ -211,13 +376,58 @@ namespace final.Controllers
         }
 
 
-    [HttpGet]
-    [Route("/Home/Admin/logout")]
-        public async Task<IActionResult> Logout()
+        [HttpGet]
+        [Route("/Home/Admin/logout")]
+        public IActionResult Logout()
  
         {
-            await HttpContext.SignOutAsync();
+            _accessor.HttpContext.Session.Clear();
             return Ok(new { message = "Logout successful." });
+        }
+
+
+        [HttpGet]
+        [Route("/Home/Admin/GetUserDevices")]
+        public IActionResult GetUserDevices()
+        {
+           string userIdStr = _accessor.HttpContext.Session.GetString("UserId");
+    // Convert the user ID to an integer
+        if (!int.TryParse(userIdStr, out int userId))
+        {
+            // Handle invalid user ID if necessary
+            return BadRequest("Invalid user ID");
+        }
+        // Fetch the devices assigned to the user
+        var devices = (from e in _context.Employee
+                    join a in _context.Assigned on e.Emp_Code equals a.Emp_Code
+                    join i in _context.Inventory on a.Id equals i.Id
+                    join d in _context.Devices on i.D_Id equals d.D_Id
+                    where e.U_Id == userId
+                    select new
+                    {
+                        serialNo = i.Serial,
+                        deviceType = d.D_Type,
+                        specification = i.Specifications
+                    }).ToList();
+                    return Json(devices);
+        }
+
+        [HttpGet]
+        [Route("/Home/test/devices")]
+        public ActionResult<IEnumerable<string>> GetDevices(string search)
+        {
+            if (string.IsNullOrWhiteSpace(search))
+            {
+                // If the search keyword is empty or null, return an empty list
+                return new List<string>();
+            }
+            // Perform a case-insensitive search on the mock devices data
+          var matchingDevices = _context.Devices
+                .AsEnumerable()
+                .Where(device => device.D_Type.Contains(search, StringComparison.OrdinalIgnoreCase) )
+                .Select(device => device.D_Type)
+                .ToList();
+            return Json(matchingDevices);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
